c75497ac6e84b643b4b178e8118a462976476a9d
MARTINOTTI Davide 71735 <davide.martinotti@consulenti.csi.it>
2009-07-29 14:41:06 +0000

    STDMDD-278: commit iniziale
    git-svn-id: http://subversion.csi.it:10090/workarea/mddtools/guigen/trunk@639 be04b910-fd5c-0410-891f-bca81b977169

---
+++ src/it/csi/mddtools/guigen/genutils/newrupar/GenUtilsLayoutNewrupar.java
@@ -0,0 +1,248 @@
+package it.csi.mddtools.guigen.genutils.newrupar;
+
+import it.csi.mddtools.guigen.Button;
+import it.csi.mddtools.guigen.Column;
+import it.csi.mddtools.guigen.CommandPanel;
+import it.csi.mddtools.guigen.CommandStyles;
+import it.csi.mddtools.guigen.Field;
+import it.csi.mddtools.guigen.FormPanel;
+import it.csi.mddtools.guigen.GUIModel;
+import it.csi.mddtools.guigen.HorizontalFlowPanelLayout;
+import it.csi.mddtools.guigen.MessageSeverity;
+import it.csi.mddtools.guigen.MsgBoxPanel;
+import it.csi.mddtools.guigen.PanelLayout;
+import it.csi.mddtools.guigen.PlainText;
+import it.csi.mddtools.guigen.SimpleType;
+import it.csi.mddtools.guigen.Table;
+import it.csi.mddtools.guigen.TextField;
+import it.csi.mddtools.guigen.Type;
+import it.csi.mddtools.guigen.TypedArray;
+import it.csi.mddtools.guigen.UDLRCPanelLayout;
+import it.csi.mddtools.guigen.UDLRCSpecConstants;
+import it.csi.mddtools.guigen.UDLRCWidgetLayoutSpec;
+import it.csi.mddtools.guigen.VerticalFlowPanelLayout;
+
+import it.csi.mddtools.guigen.genutils.GenUtils;
+import it.csi.mddtools.guigen.genutils.GenUtilsLayout;
+
+
+/**
+ * Questa classe contiene i metodi di utilit&agrave; per la gestione di
+ * alcuni aspetti del layout per il portale New Rupar.
+ *
+ * @author Davide Martinotti
+ */
+public class GenUtilsLayoutNewrupar {
+
+
+	/**
+	 * Ho un FormPanel di primo livello con layout UDLRC: quante colonne imposto sull'HTML?
+	 * - [U][D] L R C -> layout 3 colonne
+	 * - [U][D] L C   -> layout 2 colonne
+	 * - [U][D] L R   -> combinazione illegale (check)
+	 * - [U][D] C     -> layout 1 colonna
+	 * 
+	 * @param firstLevPanel
+	 * @param model La radice (GUIModel) del modello, necessaria a ricavare il tipo di portale.
+	 * @return
+	 * @author [DM]
+	 */
+	public static int getColumnsLayout(FormPanel firstLevPanel, GUIModel model) {
+		PanelLayout currPanLay = firstLevPanel.getLayout();
+		int columns = 1;
+		
+		if ( currPanLay instanceof VerticalFlowPanelLayout ) {
+			columns = 1;
+		} else if ( currPanLay instanceof UDLRCPanelLayout ) {
+			columns = getUDLRCColumnsLayout(firstLevPanel);
+		} else  {
+			throw new IllegalArgumentException("Layout " + currPanLay.getClass().getName() + " non gestito al primo livello");
+		}
+
+		return columns;
+	}
+
+
+	/**
+	 * Restituisce lo stile del <div> per i Button
+	 *  
+	 * @param model La radice (GUIModel) del modello, necessaria a ricavare il tipo di portale.
+	 * @param b Il Button da gestire
+	 * @return
+	 * @author [DM]
+	 */
+	public static String getButtonDivStyleByLayout(GUIModel model, Button b) {
+		String res = "";
+		
+		// TODO: IMPLEMENTARE SE NECESSARIO O ELIMINARE
+
+		return res;
+	}
+
+
+	/**
+	 * Restituisce lo stile per i Button.
+	 * Non mi piace molto questa implementazione (troppo empirica), ma...
+	 * 
+	 * @param model La radice (GUIModel) del modello, necessaria a ricavare il tipo di portale.
+	 * @param b Il Button da gestire
+	 * @return
+	 * @author [DM]
+	 */
+	public static String getButtonStyleByLayout(GUIModel model, Button b) {
+		String btnStyleT = "";
+		
+		// TODO: IMPLEMENTARE SE NECESSARIO O ELIMINARE
+		
+		return btnStyleT;
+	}
+
+
+	/**
+	 * Restituisce lo stile per i TextField.
+	 * 
+	 * @param model La radice (GUIModel) del modello, necessaria a ricavare il tipo di portale.
+	 * @param t Il widget di tipo TextFiled da gestire
+	 * @return
+	 * @author [DM] STDMDD-188
+	 */
+	public static String getTextFieldStyleByLayout(GUIModel model, TextField t) {
+		String res = "";
+		
+		// TODO: IMPLEMENTARE SE NECESSARIO O ELIMINARE
+		
+		return res;
+	}
+
+
+	/**
+	 * Restituisce lo stile di un MsgBoxPanel.
+	 * 
+	 * @param model La radice (GUIModel) del modello, necessaria a ricavare il tipo di portale.
+	 * @param mbp
+	 * @return
+	 * @author [DM]
+	 */
+	public static String getMsgBoxPanelStyleByPortal(GUIModel model, MsgBoxPanel mbp) {
+		String res = "";
+		
+		// TODO: IMPLEMENTARE SE NECESSARIO O ELIMINARE
+		
+		return res;
+	}
+
+
+	/**
+	 * Restituisce lo stile di una colonna sulla base del tipo bindato alla colonna stessa.
+	 * 
+	 * @param col
+	 * @param t
+	 * @param model La radice (GUIModel) del modello, necessaria a ricavare il tipo di portale.
+	 * @return
+	 * @author [DM]
+	 */
+	public static String getColumnStyle(Column col, Table table, GUIModel model) {
+		String res = "";
+		
+		// TODO: IMPLEMENTARE SE NECESSARIO O ELIMINARE
+
+		return res;
+	}
+
+
+	/**
+	 * Restituisce la classe per un CheckBox
+	 * NOTA: Anche se il codice è semplice (e potrebbe stare in .ext) DEVE rimanere qui
+	 *       in Java poichè questo metodo è chiamato anche da GenUtils.
+	 *       
+	 * @param model La radice (GUIModel) del modello, necessaria a ricavare il tipo di portale.
+	 * @return
+	 * @author [DM]
+	 */
+	public static String getCheckboxPortalStyle(GUIModel model) {
+		String res = "";
+		
+		// TODO: IMPLEMENTARE SE NECESSARIO O ELIMINARE
+		
+		return res;
+	}
+
+
+	/**
+	 * Restituisce la classe da applicare ad una cella nella CustomComponent sulla base
+	 * del tipo di dato del Widget (PlainText).
+	 * Utilizzato per allineare a destra i dati numerici.
+	 * 
+	 * @param model La radice (GUIModel) del modello, necessaria a ricavare il tipo di portale.
+	 * @param w Il widget di tipo PlainText da gestire
+	 * @return
+	 * @author [DM]
+	 */
+	public static String getCustomComponentColumnStyleByPortal(GUIModel model, PlainText w) {
+		String res = "";
+		
+		// TODO: IMPLEMENTARE SE NECESSARIO O ELIMINARE
+		
+		return res;
+	}
+
+
+	/**
+	 * Restituisce la classe per un TextField in una Table.
+	 * NOTA: Anche se il codice è semplice (e potrebbe stare in .ext) DEVE rimanere qui
+	 *       in Java poichè questo metodo è chiamato anche da GenUtils.
+	 *  
+	 * @param model La radice (GUIModel) del modello, necessaria a ricavare il tipo di portale.
+	 * @return
+	 * @author [DM]
+	 */	
+	public static String getColumnEditableTextfieldPortalStyle(GUIModel model) {
+		String res = "";
+		
+		// TODO: IMPLEMENTARE SE NECESSARIO O ELIMINARE
+		
+		return res;
+	}
+
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	// PRIVATE METHODS (se necessari)
+
+	/**
+	 * 
+	 * @param firstLevPanel
+	 * @return
+	 */
+	private static int getUDLRCColumnsLayout(FormPanel firstLevPanel) {
+		int columns = 1;
+
+		int left = GenUtilsLayout.getSubPanelsByLayout(firstLevPanel, UDLRCSpecConstants.LEFT).size();
+		int right = GenUtilsLayout.getSubPanelsByLayout(firstLevPanel, UDLRCSpecConstants.RIGHT).size();
+		
+		if ( right == 1 ) {
+			// layout 3 colonne
+			columns = 3;
+		} else if ( left == 1 ) {
+			// layout 2 colonne
+			columns = 2;
+		} else {
+			// layout 1 colonna
+		}
+		
+		return columns;
+	}
+
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	// MAIN
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
